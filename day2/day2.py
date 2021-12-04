aeff175763043a78b9977dd34608c9724bbdb455
Dean Jiao <deanjiao@gmail.com>
2021-12-03 20:14:08 -0500

    day2 part2

--- day2/day2.py
+++ day2/day2.py
@@ -15,3 +15,23 @@ for line in f:
 
 result = horizontal * depth
 print(result)
+
+# PART 2
+
+f = open('day2/input.txt', 'r')
+horizontal = 0
+depth = 0
+aim = 0
+
+for line in f:
+    direction, x = line.split()
+    if direction == 'forward':
+        horizontal += int(x)
+        depth += aim * int(x)
+    elif direction == 'down':
+        aim += int(x)
+    elif direction == 'up':
+        aim -= int(x)
+
+result = horizontal * depth
+print(result)
